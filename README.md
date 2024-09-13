1c35d618cc5ae8ba0241484023a7e86dce3fa128
Xabier Larrakoetxea <me@slok.dev>
2024-09-13 18:07:16 +0200

    Improve showcase command with more examples
    Signed-off-by: Xabier Larrakoetxea <me@slok.dev>

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Stactus
+
+## Features
+
+- Simple architecture (single binary).
+- Easy to deploy and operate.
+- Based on static configuration (like a git repository).
+- Customizable.
+
+## Deploy strategies
+
+- [Example with Github actions deploying to status pages](https://github.com/slok/stactus-test).
+
+### Load from Git repository
+
+### Build your own docker image
--- cmd/stactus/commands/showcase.go
+++ cmd/stactus/commands/showcase.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/alecthomas/kingpin/v2"
 	"github.com/oklog/run"
+	"golang.org/x/sync/errgroup"
 
 	appgenerate "github.com/slok/stactus/internal/app/generate"
 	"github.com/slok/stactus/internal/dev"
@@ -51,16 +52,28 @@ func (c *ShowcaseCommand) Run(ctx context.Context) (err error) {
 	}
 
 	statusPageClients := []client{
+		{Name: "1Password", Path: "onepassword", URL: "https://status.1password.com/"},
+		{Name: "Atlassian Statuspage", Path: "atlassianstatuspage", URL: "https://metastatuspage.com/"},
+		{Name: "Bambulab", Path: "bambulab", URL: "https://status.bambulab.com/"},
 		{Name: "Cloudflare", Path: "cloudflare", URL: "https://www.cloudflarestatus.com/"},
 		{Name: "Datadog", Path: "datadog", URL: "https://status.datadoghq.com/"},
 		{Name: "Digital Ocean", Path: "digitalocean", URL: "https://status.digitalocean.com/"},
 		{Name: "Discord", Path: "discord", URL: "https://discordstatus.com/"},
+		{Name: "Figma", Path: "figma", URL: "https://status.figma.com/"},
+		{Name: "FlyIO", Path: "flyio", URL: "https://status.flyio.net/"},
 		{Name: "Github", Path: "github", URL: "https://www.githubstatus.com/"},
 		{Name: "Grafana", Path: "grafana", URL: "https://status.grafana.com/"},
 		{Name: "Hashicorp", Path: "hashicorp", URL: "https://status.hashicorp.com/"},
 		{Name: "MIT", Path: "mit", URL: "https://atlas-status.mit.edu/"},
+		{Name: "MongoDB", Path: "mongodb", URL: "https://status.mongodb.com/"},
+		{Name: "New Relic", Path: "newrelic", URL: "https://status.newrelic.com/"},
+		{Name: "Reddit", Path: "reddit", URL: "https://www.redditstatus.com/"},
 		{Name: "RedisLabs", Path: "redislabs", URL: "https://status.redis.io/"},
 		{Name: "Twilio", Path: "twilio", URL: "https://status.twilio.com/"},
+		{Name: "Twitch", Path: "twitch", URL: "https://status.twitch.com"},
+		{Name: "Uber", Path: "uber", URL: "https://flgtt5cfx545.statuspage.io/"},
+		{Name: "Ubiquiti", Path: "ubiquiti", URL: "https://status.ui.com/"},
+		{Name: "Zoom", Path: "zoom", URL: "https://status.zoom.us/"},
 	}
 
 	themes := []string{
@@ -76,7 +89,6 @@ func (c *ShowcaseCommand) Run(ctx context.Context) (err error) {
 		g.Add(
 			func() error {
 				<-ctx.Done()
-				logger.Infof("Context cancelled...")
 				return nil
 			},
 			func(err error) {
@@ -85,112 +97,110 @@ func (c *ShowcaseCommand) Run(ctx context.Context) (err error) {
 		)
 	}
 
-	// Add showcase simple index file.
-	{
-		g.Add(
-			func() error {
-				indexFile := path.Join(c.outPath, "index.html")
+	// Add static pages generation.
+	g.Add(
+		func() error {
 
-				showcaseLinks := ""
-				for _, client := range statusPageClients {
-					for _, theme := range themes {
-						url := c.siteURL + "/" + theme + "/" + client.Path
-						name := fmt.Sprintf("%s (%s)", client.Name, theme)
-						showcaseLinks += fmt.Sprintf(`<div><a href="%s">%s</a><div>`, url, name)
-					}
+			// Generate simple index file.
+			indexFile := path.Join(c.outPath, "index.html")
+
+			showcaseLinks := ""
+			for _, client := range statusPageClients {
+				for _, theme := range themes {
+					url := c.siteURL + "/" + theme + "/" + client.Path
+					name := fmt.Sprintf("%s (%s)", client.Name, theme)
+					showcaseLinks += fmt.Sprintf(`<div><a href="%s">%s</a><div>`, url, name)
 				}
+			}
 
-				err := os.WriteFile(indexFile, []byte(`<html>
+			err := os.WriteFile(indexFile, []byte(`<html>
 				<head><title>Stactus showcase</title></head>
 				<body>
 				<h1>Showcase</h1>
 				<p>`+showcaseLinks+`</p>
 				</body>
 				</html>`), 0666)
-				if err != nil {
-					return err
-				}
+			if err != nil {
+				return err
+			}
 
-				return nil
-			},
-			func(err error) {
-				cancel(err)
-			},
-		)
-	}
+			// Generate all themes and clients status pages.
+			group, ctx := errgroup.WithContext(ctx)
+			group.SetLimit(5)
 
-	// Add clietn generators.
-	for _, client := range statusPageClients {
-		client := client
+			for _, client := range statusPageClients {
+				group.Go(func() error {
+					client := client
 
-		g.Add(
-			func() error {
-				logger.Infof("Generating %s example", client.Name)
+					logger.Infof("Generating %s example", client.Name)
 
-				// Setup repositories.
-				devRepo, err := dev.NewStatusPageRepository(client.URL)
-				if err != nil {
-					return fmt.Errorf("could not create repository: %w", err)
-				}
+					// Setup repositories.
+					devRepo, err := dev.NewStatusPageRepository(client.URL)
+					if err != nil {
+						return fmt.Errorf("could not create repository: %w", err)
+					}
 
-				// Render a client per theme.
-				for _, theme := range themes {
-					outPath := path.Join(c.outPath, theme, client.Path)
-					siteURL := c.siteURL + "/" + theme + "/" + client.Path
+					// Render a client per theme.
+					for _, theme := range themes {
+						outPath := path.Join(c.outPath, theme, client.Path)
+						siteURL := c.siteURL + "/" + theme + "/" + client.Path
 
-					var uiCreator storage.UICreator
-					switch theme {
-					case themeBase:
-						uiCreator, err = htmlbase.NewGenerator(htmlbase.GeneratorConfig{
-							OutPath: outPath,
-							SiteURL: siteURL,
-							Logger:  logger,
-							ThemeCustomization: htmlbase.ThemeCustomization{
-								BrandTitle: client.Name,
-								BrandURL:   client.URL,
-							},
-						})
-						if err != nil {
-							return fmt.Errorf("could not create HTML generator: %w", err)
+						var uiCreator storage.UICreator
+						switch theme {
+						case themeBase:
+							uiCreator, err = htmlbase.NewGenerator(htmlbase.GeneratorConfig{
+								OutPath: outPath,
+								SiteURL: siteURL,
+								Logger:  logger,
+								ThemeCustomization: htmlbase.ThemeCustomization{
+									BrandTitle: client.Name,
+									BrandURL:   client.URL,
+								},
+							})
+							if err != nil {
+								return fmt.Errorf("could not create HTML generator: %w", err)
+							}
+
+						case themeSimple:
+							uiCreator, err = htmlsimple.NewGenerator(htmlsimple.GeneratorConfig{
+								OutPath: outPath,
+								SiteURL: siteURL,
+								Logger:  logger,
+								ThemeCustomization: htmlsimple.ThemeCustomization{
+									BrandTitle: client.Name,
+									BrandURL:   client.URL,
+								},
+							})
+							if err != nil {
+								return fmt.Errorf("could not create HTML generator: %w", err)
+							}
 						}
 
-					case themeSimple:
-						uiCreator, err = htmlsimple.NewGenerator(htmlsimple.GeneratorConfig{
-							OutPath: outPath,
-							SiteURL: siteURL,
-							Logger:  logger,
-							ThemeCustomization: htmlsimple.ThemeCustomization{
-								BrandTitle: client.Name,
-								BrandURL:   client.URL,
-							},
+						// Generator service.
+						genService, err := appgenerate.NewService(appgenerate.ServiceConfig{
+							SystemGetter: devRepo,
+							IRGetter:     devRepo,
+							UICreator:    uiCreator,
+							Logger:       logger,
 						})
 						if err != nil {
-							return fmt.Errorf("could not create HTML generator: %w", err)
+							return fmt.Errorf("could not create generation service: %w", err)
+						}
+
+						_, err = genService.Generate(ctx, appgenerate.GenerateReq{})
+						if err != nil {
+							return fmt.Errorf("generation failed: %w", err)
 						}
 					}
 
-					// Generator service.
-					genService, err := appgenerate.NewService(appgenerate.ServiceConfig{
-						SystemGetter: devRepo,
-						IRGetter:     devRepo,
-						UICreator:    uiCreator,
-						Logger:       logger,
-					})
-					if err != nil {
-						return fmt.Errorf("could not create generation service: %w", err)
-					}
+					return nil
+				})
+			}
 
-					_, err = genService.Generate(ctx, appgenerate.GenerateReq{})
-					if err != nil {
-						return fmt.Errorf("generation failed: %w", err)
-					}
-				}
-
-				return nil
-			},
-			func(err error) {},
-		)
-	}
+			return group.Wait()
+		},
+		func(err error) {},
+	)
 
 	return g.Run()
 }
